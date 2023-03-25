da104ffe0afa25ddf9f54cc16d138851a3e370dd
Truc Duong <trucdg@bu.edu>
2023-03-24 20:28:24 -0400

    use CSS module to scope the stylings to the button components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React from "react";
+// import styled from "styled-components";
+import styles from "./Button.module.css";
 
-import './Button.css';
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
-const Button = props => {
+//attacked template literal
+//button is a method on this object
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+
+//   @media (min-width: 786px) {
+//     width: auto;
+//   }
+// `;
+
+// this will return a Button object with the style we provided between the back ticks
+
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
